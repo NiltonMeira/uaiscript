38b843a7517f92582c3e596206954d60a2057f29
NiltonMeira <murylosaladino@gmail.com>
2023-10-20 15:32:38 -0300

    fix compiler

--- compiler/main.py
+++ compiler/main.py
@@ -5,6 +5,78 @@ import os
 def compile (id, message):
  return "{" + f"id: {id}, parameters: [{message}]" + "}"
 
+path = "./code"
+caminhos = os.listdir(path)
+main = list(filter(lambda x : x == "script.uai", caminhos))
+
+with open(path+"/"+main[0], "r") as arquivo:
+ arichive = arquivo.read()
+
+# context = arichive.replace("\n", "").split(";")
+context = arichive.split("\n")
+
+print(context)
+print("============")
+
+toJs = "export const execute = ["
+_if = False
+ifLines = 0
+
+for item in context:
+ if item == "":
+  continue
+ id = 0
+ message = ""
+
+ if item.find("}") > 0:
+   _if = False
+   ifLines = 0
+   continue
+ if _if:
+   ifLines += 1
+
+  
+ # verifica se atribuição
+ elif item.find("=") > 0:
+  declaration = item.replace(" ","").split("=")
+  id = 0
+  message = f"'{declaration[0]}', {declaration[1]}"
+
+
+ #verifica se é function
+ elif item.find("(") > 0:
+  print(f"{item}")
+  function = item.replace(" ","").split("(")
+  match function[0]:
+   case 'tremQueMostra':
+     id = 1
+     message = function[1].replace(")" , "").replace(";","")
+     
+   case 'tremQueCheca':
+     id = 2
+     end =False
+     message = function[1].replace(")" , "").replace(";","").replace("{","") + f"{ifLines}"
+
+ toJs += compile(id, message).replace(";","") + ","
+
+
+toJs = toJs[:len(toJs)-1] + "]" 
+toJs += "\n \nexport const values = []"
+print(toJs)
+
+    
+
+
+outputPath = "./engine/output.mjs"
+output = open(outputPath, "w")
+output.write(toJs)
+import os
+
+
+
+def compile (id, message):
+ return "{" + f"id: {id}, parameters: [{message}]" + "}"
+
 path = "./target"
 caminhos = os.listdir(path)
 main = list(filter(lambda x : x == "script.uai", caminhos))
@@ -55,16 +127,9 @@ for item in context:
    case 'tremQueCheca':
      id = 2
      end =False
-     while( not end ):
-       for i in range(len(context)):
-         if context[i].find("}") > 0:
-           ifLines += 1
-           end = False
-           break
-     
      message = function[1].replace(")" , "").replace(";","").replace("{","") + f"{ifLines}"
 
- toJs += compile(id, message) + ","
+ toJs += compile(id, message).replace(";","") + ","
 
 
 toJs = toJs[:len(toJs)-1] + "]" 
--- engine/output.mjs
+++ engine/output.mjs
@@ -1,3 +1,3 @@
-export const execute = []
-
+export const execute = [{id: 0, parameters: ['linha', "oi"]},{id: 0, parameters: ['numero', 12]},{id: 0, parameters: ['varresult', 1+2+3]},{id: 0, parameters: []}]
+ 
 export const values = []
